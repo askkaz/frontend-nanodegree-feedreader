5571d52070fdddec4c76c93ecf94791f05884812
askkaz <askkazgithub@gmail.com>
2015-05-23 15:36:34 -0400

    Add comments to feedreader.js.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -5,19 +5,23 @@
  */
 
  $(function() {
+    /*
+    * This first suite of tests is on the RSS Feeds.  They ensure
+    * that there exists at least one feed, that each feed has a
+    * URL defined that is not empty, and that each feed has a name
+    * defined that is not empty..
+    */
     describe('RSS Feeds', function() {
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
         it('has a URL defined for each feed', function() {
             for (i = 0 ; i<allFeeds.length; i++){
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
             }
         });
-
         it('has a name defined for each feed', function() {
             for (i = 0 ; i<allFeeds.length; i++){
                 expect(allFeeds[i].name).toBeDefined();
@@ -25,7 +29,12 @@
             }
         });
     });
-
+    /*
+    * This suite of tests is on the menu.  The menu starts hidden
+    * when the page is loaded.  When the menu icon is first clicked,
+    * the menu should appear.  When it is clicked again, the menu
+    * should disappear.
+    */
     describe('The menu', function(){
         it('is hidden',function(){
             expect($('body').hasClass('menu-hidden')).toBe(true);
@@ -39,7 +48,12 @@
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
-
+    /*
+    * This suite of tests is on the initial entries populated for the
+    * first feed.  It ensures that there is at least a single entry after
+    * the async request, loadFeed().  beforeEach and done() are used to
+    * ensure jasmine waits for the request to complete before continuing.
+    */
     describe('Initial Entries', function(){
         beforeEach(function(done) {
             loadFeed(0, function (){
@@ -50,7 +64,16 @@
             expect($('.feed').find('.entry').length).not.toBe(0);
         });
     });
-
+    /*
+    * This suite of tests is to ensure that selecting a new feed changes
+    * the displayed feed.  It first loads the initial feed at index 0,
+    * using loadFeed.  The resulting feed text displayed to the user
+    * is stored in oldContent.  The feed is then changed to the feed at
+    * index 1, again using loadFeed.  The new feed text displayed to the
+    * user is then compared to the oldContent.  beforeEach and done()
+    * are used to ensure jasmine waits for the loadFeed request completes
+    * before continuing.
+    */
     describe('New Feed Selection', function(){
         beforeEach(function(done) {
             loadFeed(0, function (){
